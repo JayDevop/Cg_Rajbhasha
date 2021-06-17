933a7e256e74ce5a62abc097737c1946f0d1a8ec
Pragyesh <pragyeshnagvanshi@gmail.com>
2021-06-17 16:12:30 +0530

    Update video_gallery.php

--- application/views/video_gallery.php
+++ application/views/video_gallery.php
@@ -1,56 +1,60 @@
 
 
-  <main id="main">
+<main id="main">
 
     <!-- ======= Breadcrumbs ======= -->
     <div class="breadcrumbs" data-aos="fade-in">
-      <div class="container">
-      
-        <h2>वीडियो ग़ैलरी</h2>
-        
-      </div>
+        <div class="container">
+            <h2>वीडियो ग़ैलरी</h2>
+        </div>
     </div><!-- End Breadcrumbs -->
 
     <!-- ======= Events Section ======= -->
-    <section id="events" class="events">
-      <div class="container" data-aos="fade-up">
+    <section id="trainers" class="trainers">
+        <div class="container" data-aos="fade-up">
 
-      <div class="row">
-        <div class="col-sm-4">
-      <div class="card" style="width: 18rem;">
-  <video src="https://codingyaar.com/wp-content/uploads/video-in-bootstrap-card.mp4" style="max-width: 100%;" controls></video>
-  <div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
-        <div class="col-sm-4">     
-<div class="card" style="width: 18rem;">
-  <video src="https://codingyaar.com/wp-content/uploads/video-in-bootstrap-card.mp4"></video>
-  <div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+            <div class="row" data-aos="zoom-in" data-aos-delay="100">
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <video width="350" controls>
+                            <source src="https://codingyaar.com/wp-content/uploads/video-in-bootstrap-card.mp4" type="video/mp4">
+                            <!-- <source src="mov_bbb.ogg" type="video/ogg"> -->
+                            Your browser does not support HTML video.
+                        </video>
+                        <div class="member-content">
+                            <p>Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <video width="350" controls>
+                            <source src="https://codingyaar.com/wp-content/uploads/video-in-bootstrap-card.mp4" type="video/mp4">
+                            <!-- <source src="mov_bbb.ogg" type="video/ogg"> -->
+                            Your browser does not support HTML video.
+                        </video>
+                        <div class="member-content">
+                            <p>Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <video width="350" controls>
+                            <source src="https://codingyaar.com/wp-content/uploads/video-in-bootstrap-card.mp4" type="video/mp4">
+                            <!-- <source src="mov_bbb.ogg" type="video/ogg"> -->
+                            Your browser does not support HTML video.
+                        </video>
+                        <div class="member-content">
+                            <p>Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-sm-4">
-<div class="card" style="width: 18rem;">
-  <video src="https://codingyaar.com/wp-content/uploads/video-in-bootstrap-card.mp4"></video>
-  <div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div></div>
-      </div>
-  
-   
-    </div>
     </section><!-- End Events Section -->
-
-  </main><!-- End #main -->
+</main><!-- End #main -->
 
   
